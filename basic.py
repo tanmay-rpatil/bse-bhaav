9e3b6abcaed68117e9a49c64836528141de21512
tanmay-rpatil <f20190054@goa.bits-pilani.ac.in>
2021-04-01 19:39:20 +0530

    cleaned up code, comments

--- basic.py
+++ basic.py
@@ -1,31 +1,36 @@
 import requests, csv
 import datetime
 from zipfile import ZipFile
-h = {'user-agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0"}
 
+# global vars
+
+#headers for mimicking a browser
+h = {'user-agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0"}
+# generate today's date
 date  = datetime.datetime.now()
 d = date.strftime("%d")
 d += date.strftime("%m")
 d += date.strftime("%y")
-# d = '300321'
-print(d)
+# d = '300321' => for debugging purposes
+print(d) 
+
+# urls, downloaded zip files 
 fname = d+"_CSV.zip"
 link = "http://www.bseindia.com/download/BhavCopy/Equity/EQ"+fname
 print(link)
 
 
+# main
 r = requests.get(link, headers=h)
-
 if	r.status_code != 200:
 	print(str(r.status_code)+" error, exiting program")
 	exit()
 else:
 	print(str("parsing info for the date- " + d ))
-# print(r.headers)
-# print(r)
+
+#giving a name and saving it in zip format
 with open(fname, 'wb') as f:
-#giving a name and saving it in any required format
-#opening the file in write mode
+
 	f.write(r.content)
 	# print(r)
 
@@ -33,10 +38,10 @@ with ZipFile(fname, 'r') as zipObj:
    # Extract all the contents of zip file in current directory
 	zipObj.extractall()
 
-#get the required codes in list
+#get the required stock codes in list
 sc_codes_lst = []
 
-# get the reuired codes of shares
+# get the required codes of shares
 with open("config.dat") as f:
 	for line in f.readlines():
 		if len(line)!=0:
