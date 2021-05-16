3d30b815063b2118fd038256ac78167e5f05c839
tanmay-rpatil <f20190054@goa.bits-pilani.ac.in>
2021-05-16 10:50:55 +0530

    ported old code to functions

--- download_req.py
+++ download_req.py
@@ -41,10 +41,48 @@ def download(request,ftype,fname):
 	#giving a name and saving it in any required format, opening the file in write mode
 		print("downloading: " + fname )
 		f.write(request.content)
-	if (ftype=="zip"):
-		with ZipFile(fname, 'r') as zipObj:
-		# Extract all the contents of zip file in current directory
-			zipObj.extractall()
+
+#extract zip file with given name
+def extract_zip(fname):
+	with ZipFile(fname, 'r') as zipObj:
+    # Extract all the contents of zip file in current directory
+		zipObj.extractall()
+
+# process ip files, and give an output
+def proc(ip_fname,req_fname,op_fname):
+	# get target codes from req_fname 
+	# get the reuired codes of stocks
+	sc_codes_lst = []
+	with open(req_fname) as f:
+		for line in f.readlines():
+			if len(line)!=0:
+				sc_codes_lst.append(line.strip())
+	
+	valid_tuples = {}
+	titles = ['SC_CODE', 'SC_NAME', 'HIGH', 'LOW', 'OPEN', 'CLOSE','LAST']
+	with open('output.csv', 'w', newline='' ) as op_file:
+		writer = csv.writer(op_file)
+		writer.writerow(titles)
+		for code in sc_codes_lst:
+			with open(op_fname, newline='') as csvfile:
+				csv_reader = csv.DictReader(open(ip_fname), delimiter=',')
+				found = False
+				print(ip_fname)
+				# print(csv_reader)
+				for row in csv_reader:
+					print(row)
+					if row.get('SC_CODE')==code:
+						found = True
+						data_lst = []
+						for title in titles:
+							data_lst.append(row.get(title).strip())
+						print(data_lst)
+						writer.writerow(data_lst)
+						break;
+				if not found:
+					print("not found-> " + code)
+
+
 
 def main():
 	print ("Input required date in the format ddmmyy (ex 300321 for 30th may 2001) \n OR ")
@@ -55,10 +93,12 @@ def main():
 	if	req.status_code != 200:
 		print(str(req.status_code)+" error, exiting program")
 		exit()
-	else:
-		print(str("parsing info for the date- " + date ))
-		fname = date+"_CSV.ZIP"
-		download(req,"zip",fname)
+	# if code is 200 ie req is success	
+	print(str("parsing info for the date- " + date ))
+	fname = date+"_CSV.ZIP"
+	download(req,"zip",fname)
+	extract_zip(fname)
+	proc("EQ"+date+".CSV","config.dat","output.csv")
 
 
 if __name__ == "__main__":
