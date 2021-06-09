565fff45a55a6d1b90c9789595b2a1562afbc442
tanmay-rpatil <f20190054@goa.bits-pilani.ac.in>
2021-06-09 21:38:38 +0530

    testing command line arguments

---
+++ try.py
@@ -0,0 +1,9 @@
+import argparse
+
+# print ('#of args =', len(sys.argv), 'arguments.')
+if __name__=='__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('echo',help='echo the string entered')
+	args = parser.parse_args()
+	print(args.echo)    
+	
